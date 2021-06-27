1bbdc4c3a2cac019293fad36e0afb31dbecf66ac
Akash-213 <akash213kulkarni@gmail.com>
2021-06-27 15:52:20 +0530

    explore page version 1

--- lib/screens/tabs/explore_tab.dart
+++ lib/screens/tabs/explore_tab.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:sig_platform/widgets/loading_widget.dart';
 
 class ExploreTab extends StatefulWidget {
   const ExploreTab({Key? key}) : super(key: key);
@@ -12,26 +14,24 @@ class _ExploreTabState extends State<ExploreTab> {
   @override
   Widget build(BuildContext context) {
     final currUser = FirebaseAuth.instance.currentUser!;
-    return Container(
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CircleAvatar(
-              radius: 50,
-              backgroundImage: NetworkImage(currUser.photoURL ?? ''),
-            ),
-            Text(
-              currUser.displayName ?? 'DisplayName',
-              style: TextStyle(fontSize: 30),
-            ),
-            Text(
-              currUser.email ?? 'Email',
-              style: TextStyle(fontSize: 20),
-            ),
-          ],
-        ),
-      ),
-    );
+    return StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('sigs').snapshots(),
+        builder: (_, snapshot) {
+          if (snapshot.data == null ||
+              snapshot.connectionState == ConnectionState.waiting) {
+            return LoadingWidget();
+          }
+
+          final sigDocs = snapshot.data!.docs;
+          print(sigDocs[0]['sigTitle']);
+          return ListView.builder(
+              itemCount: sigDocs.length,
+              itemBuilder: (_, index) {
+                return Card(
+                  child: Text(sigDocs[index]['sigTitle']),
+                );
+              });
+        });
+    ;
   }
 }
